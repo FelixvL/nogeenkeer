ef08407ad02a559b3b8ac039bc19d2a1c589ae7c
wjjongkind <imjustacowlolz@gmail.com>
2018-10-08 16:07:53 +0200

    tweede commit

--- Demo.java
+++ Demo.java
@@ -15,7 +15,7 @@ public class Demo {
 			System.out.println("go");
 		}
 	}
-	static void aannemen(Deelnemer deelnemer) {
+	static void aannemen(Deelnemer deelnemer) { // mooie methode
 		if(deelnemer.geschiktePersoonlijkheid && deelnemer.technischeUitslag > 50 && deelnemer.leeftijd < 35) {
 			System.out.println("U bent aangenomen");
 		}else {
